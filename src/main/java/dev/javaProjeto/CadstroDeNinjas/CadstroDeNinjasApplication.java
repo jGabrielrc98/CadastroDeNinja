e9345fcc17dcbead82c380b54e0950f23466ea81
jGabrielrc98 <jgabrielrc7@gmail.com>
2025-07-26 15:58:17 -0300

    first commit

---
+++ src/main/java/dev/javaProjeto/CadstroDeNinjas/CadstroDeNinjasApplication.java
@@ -0,0 +1,13 @@
+package dev.javaProjeto.CadstroDeNinjas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadstroDeNinjasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadstroDeNinjasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/javaProjeto/CadstroDeNinjas/CadstroDeNinjasApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.javaProjeto.CadstroDeNinjas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadstroDeNinjasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
